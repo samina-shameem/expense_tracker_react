c6cee09b2ac8b3549bf7d73bc000aa2aedece9b7
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-30 18:59:09 +0100

    remove duplicate  import addtransaction.

--- expense_tracker/src/App.jsx
+++ expense_tracker/src/App.jsx
@@ -3,9 +3,6 @@ import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
-import AddTransaction from './components/AddTransaction';
-
-
 import './App.css';
 import AddTransaction from './components/AddTransaction';
 
