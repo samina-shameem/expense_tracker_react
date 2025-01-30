5e5606e0949892819058390e46c44a6674d96eba
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-30 19:44:31 +0100

    import global provider and rap g

--- expense_tracker/src/App.jsx
+++ expense_tracker/src/App.jsx
@@ -5,10 +5,11 @@ import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
 import './App.css';
 import AddTransaction from './components/AddTransaction';
+import  GlobalProvider  from './context/GlobalState';
 
 const App = () => {
   return (
-    <div>
+    <GlobalProvider>
       <Header/>
       <div className='container'>
         <Balance/>
@@ -16,7 +17,7 @@ const App = () => {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   )
 }
 
