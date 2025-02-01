05238e027c7876525c269b0dc8faabd0e2c458ee
samina-shameem <samina.shameem.clt@gmail.com>
2025-02-01 21:52:51 +0100

    add switch statement.

--- expense_tracker/src/context/AppReducer.jsx
+++ expense_tracker/src/context/AppReducer.jsx
@@ -1,19 +1,25 @@
-export default (state, action) => {
-    switch (action.type) {
-      case 'ADD_TRANSACTION':
-        return {
+const AppReducer =(state, action) => {
+  switch (action.type){
+    case "ADD_TRANSACTION":
+      return{
+        ...state,
+        transactions:[action.payload, ...state.transactions],
+
+      };
+
+      case "DELETE_TRANSACTION":
+        return{
           ...state,
-          transactions: [action.payload, ...state.transactions],
-        };
-      case 'DELETE_TRANSACTION':
-        return {
-          ...state,
-          transactions: state.transactions.filter(
+          transactions:state.transactions.filter(
             (transaction) => transaction.id !== action.payload
+
           ),
         };
-      default:
-        return state;
-    }
-  };
-  
+        default:
+          return state;
+      
+
+  }
+};
+
+export default AppReducer;
