69d9bd7c38bbd48aa7dd33380f754d94925aaa30
samina-shameem <samina.shameem.clt@gmail.com>
2025-02-02 21:03:24 +0100

    changed some issues.

--- expense_tracker/src/components/TransactionList.jsx
+++ expense_tracker/src/components/TransactionList.jsx
@@ -5,13 +5,11 @@ import Transaction from "./Transaction";
 const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
 
-  //console.log(context);
-
   return (
     <div>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map((transaction)=>(
+        {transactions.map((transaction) => (
           <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
