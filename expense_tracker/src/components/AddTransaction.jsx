49aad4f90f45e3019f53872723e0c2c167f2791a
samina-shameem <samina.shameem.clt@gmail.com>
2025-02-02 20:30:55 +0100

    added calculations.

--- expense_tracker/src/components/AddTransaction.jsx
+++ expense_tracker/src/components/AddTransaction.jsx
@@ -1,11 +1,52 @@
-import React,{useState} from 'react';
+import React,{useState, useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 const AddTransaction = () => {
 
   const [text,setText]=useState('');
   const [amount,setAmount]=useState(0);
-  return (
+  const {AddTransaction} = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+     if(!text || amount === 0)return;
+
+     const newTransaction = {
+      id: Math.floor(Math.random() * 1000000),
+      text,
+      amount: parseFloat(amount),
+     };
+
+     AddTransaction(newTransaction);
+     setText("");
+     setAmount(0);
+
+  };
+
+  return(
+    <div>
+      <h3>Add new transaction</h3>
+        <form onSubmit={onSubmit}>
+          <div className='form-control'>
+            <label htmlFor='text'>Text</label>
+            <input type="text" vlaue={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...'/>
+          </div>
+          <div className='form-control'>
+            <label htmlFor='amount'>
+              Amount <br />
+              (negative-expense, positive-income)
+            </label>
+            <input type="number" value={amount} onChange={(e) => setAmount(+e.target.value)}placeholder='Enter amount...' />
+
+          </div>
+          <button className='btn'>Add transaction</button>
+        </form>
+    </div>
+  );
+};
+
+  /*return (
     <div>
       <h3>Add new transaction</h3>
       <form >
@@ -23,6 +64,6 @@ const AddTransaction = () => {
       </form>
       </div>
   )
-}
+}*/
 
 export default AddTransaction;
